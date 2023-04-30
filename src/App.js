acfc50616581affc7098e829a2df6b93335f62b0
BERKAY GÜL <berrkaygul@gmail.com>
2023-05-01 02:56:08 +0300

    proje kurgusu oluşturuldu

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Campaign from "./components/Campaign";
+import Categories from "./components/Categories";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import HeaderSection from "./components/HeaderSection";
+import MobileAppBar from "./components/MobileAppBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <HeaderSection />
+      <Categories />
+      <MobileAppBar />
+      <Campaign />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/MobileAppBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MobileAppBar = () => {
+  return (
+    <div>MobileAppBar</div>
+  )
+}
+
+export default MobileAppBar
